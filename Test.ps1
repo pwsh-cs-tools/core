d421ebc0263b0a637a61f6d3353408fc918a4535
Blue Falcon <anon@hostpi.onmicrosoft.com>
2023-10-22 00:33:02 -0700

    test: :test_tube: add testing script

---
+++ Test.ps1
@@ -0,0 +1,19 @@
+Import-Module "$PSScriptRoot\Import-Package\"
+
+Import-Package Avalonia.Desktop -Offline
+# Import-Package Avalonia.Win32 -Offline
+Write-Host (Get-Runtime)
+
+Import-Module "$PSScriptRoot\New-DispatchThread\"
+
+Set-DispatcherFactory ([Avalonia.Threading.Dispatcher])
+
+$t = New-DispatchThread
+$t.Invoke({ Write-Host "test - Avalonia" })
+
+Set-DispatcherFactory ([System.Windows.Threading.Dispatcher])
+
+$t = New-DispatchThread
+$t.Invoke({ Write-Host "test - WPF" })
+
+$Threads = Get-Threads
