74caefc8c7afe05e85743707967febd37ae28b18
Blue Falcon <anon@hostpi.onmicrosoft.com>
2023-11-01 11:39:32 -0700

    test: :test_tube: split Test.ps1 into 2 separate tests

--- Test.ps1
+++ Test.ps1
@@ -1,38 +1,73 @@
-Import-Module "PackageManagement"
+param(
+    [bool] $ImportPackage = $true,
+    [bool] $NewDispatchThread = $true,
+    [string] $Root = (& {
+            If( $PSScriptRoot ){
+            $PSScriptRoot
+        } Else {
+            Resolve-Path .
+        }
+    })
+)
+
 $VerbosePreference = "Continue"
 
-Import-Module "$PSScriptRoot\Import-Package\"
+If( $ImportPackage ){
+    Import-Module "PackageManagement"
+    Import-Module "$Root\Import-Package\"
 
-Import-Module "$PSScriptRoot\New-DispatchThread\"
+    # --- Avalonia ---
+    
+    Import-Package Avalonia.Desktop -Offline
+    # Import-Package Avalonia.Win32 -Offline]
 
-# --- Avalonia ---
+    Write-Host
+    Write-Host (Get-Runtime)
 
-Import-Package Avalonia.Desktop -Offline
-# Import-Package Avalonia.Win32 -Offline]
+    Pause;
+}
 
-# Should dump a warning followed by an error
-Update-DispatcherFactory ([Avalonia.Threading.Dispatcher])
+If( $NewDispatchThread ){
+    Import-Module "$Root\New-DispatchThread\"
 
-# --- ThreadExtensions ---
-Update-DispatcherFactory ([ThreadExtensions.Dispatcher])
+    # Should dump a warning followed by an error
+    Update-DispatcherFactory ([Avalonia.Threading.Dispatcher])
+    
+    # --- ThreadExtensions ---
+    Update-DispatcherFactory ([ThreadExtensions.Dispatcher])
+    
+    $t1 = New-DispatchThread
+    $t1.Invoke({ Write-Host "test - ThreadExtensions:Un-named" }).
+        Invoke({ Write-Host "done - ThreadExtensions:Un-named" }, $true) | Out-Null
+    
+    Try{
+        $t2 = New-DispatchThread -name "Tester"
+        $t2.Invoke({ Write-Host "test - ThreadExtensions:Named" }).
+            Invoke({ Write-Host "done - ThreadExtensions:Named" }, $true) | Out-Null
+    } Catch {
+        Write-Host "Caught error: $_"
+    }
+    
+    # --- WPF ---
+    
+    Write-Host
+    Update-DispatcherFactory ([System.Windows.Threading.Dispatcher])
+    
+    $t3 = New-DispatchThread
+    $t3.Invoke({ Write-Host "test - WPF:Un-named" }).
+        Invoke({ Write-Host "done - WPF:Un-named" }, $true) | Out-Null
 
-$t = New-DispatchThread
-$t.Invoke({ Write-Host "test - ThreadExtensions" }).
-    Invoke({ Write-Host "done - ThreadExtensions" }, $true) | Out-Null
-
-# --- WPF ---
-
-Write-Host
-Update-DispatcherFactory ([System.Windows.Threading.Dispatcher])
-
-$t = New-DispatchThread
-$t.Invoke({ Write-Host "test - WPF" }).
-    Invoke({ Write-Host "done - WPF" }, $true) | Out-Null
-
-Write-Host
-Write-Host (Get-Runtime)
-Write-Host
-Write-Host "Threads:"
-
-$Threads = Get-Threads
-$Threads
+    Try{ 
+        $t4 = New-DispatchThread -name "Tester"
+        $t4.Invoke({ Write-Host "test - WPF:Named" }).
+            Invoke({ Write-Host "done - WPF:Named" }, $true) | Out-Null
+    } Catch {
+        Write-Host "Caught error: $_"
+    }
+    
+    Write-Host
+    Write-Host "Threads:"
+    
+    $Threads = Get-Threads
+    $Threads
+}
